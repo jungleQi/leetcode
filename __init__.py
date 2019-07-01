3ae7f7e0740393b0cdcb765f58dba84cd69c3abd
85937023 <85937023@qq.com>
2019-07-01 23:35:45 +0800

    2019.7.1

---
+++ __init__.py
@@ -0,0 +1,5 @@
+#https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+#https://leetcode.com/problems/first-bad-version/
+#https://leetcode.com/problems/find-k-closest-elements/
+#https://leetcode.com/problems/find-k-th-smallest-pair-distance/
+#https://leetcode.com/problems/search-insert-position/
---
+++ array/binary search/34. Find First and Last Position of Element in Sorted Array.py
@@ -0,0 +1,1 @@
+def searchInsert(nums, target):
