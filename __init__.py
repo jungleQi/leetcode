e6f49f2a5d06c55c54976a960c5df471c2c1a6aa
jungle <jungle@alibaba-inc.com>
2019-07-02 20:13:50 +0800

    278 ac

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
-#https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
-#https://leetcode.com/problems/first-bad-version/
-#https://leetcode.com/problems/find-k-closest-elements/
-#https://leetcode.com/problems/find-k-th-smallest-pair-distance/
-#https://leetcode.com/problems/search-insert-position/
+https://leetcode.com/problems/first-bad-version/
+https://leetcode.com/problems/find-k-closest-elements/
+https://leetcode.com/problems/find-k-th-smallest-pair-distance/
+https://leetcode.com/problems/search-insert-position/
