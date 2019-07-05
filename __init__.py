dab12bd56111051524d17e7c4b94c39c3fb90231
jungle <jungle@alibaba-inc.com>
2019-07-05 22:00:01 +0800

    .

--- __init__.py
+++ __init__.py
@@ -1,3 +1,8 @@
 https://leetcode.com/problems/find-k-th-smallest-pair-distance/
 https://leetcode.com/problems/search-insert-position/
-
+https://leetcode.com/problems/find-k-pairs-with-smallest-sums/
+https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
+https://leetcode.com/problems/kth-smallest-number-in-multiplication-table/
+https://leetcode.com/problems/k-th-smallest-prime-fraction/
+https://leetcode.com/problems/split-array-largest-sum/
+https://leetcode.com/problems/swim-in-rising-water/
