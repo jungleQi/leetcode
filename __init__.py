8a3ac5e553c4bfa09b4bda7b8960cd86cc93d414
jungle <jungle@alibaba-inc.com>
2019-07-08 14:16:05 +0800

    378 AC

--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
-https://leetcode.com/problems/find-k-th-smallest-pair-distance/
 https://leetcode.com/problems/search-insert-position/
 https://leetcode.com/problems/find-k-pairs-with-smallest-sums/
 https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
