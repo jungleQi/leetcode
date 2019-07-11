79ccc92c9afd53cf9a9c2232bea052fa2641c6e6
85937023 <85937023@qq.com>
2019-07-11 23:09:22 +0800

    .

--- __init__.py
+++ __init__.py
@@ -1,6 +1,4 @@
-https://leetcode.com/problems/find-k-th-smallest-pair-distance/
 https://leetcode.com/problems/search-insert-position/
-https://leetcode.com/problems/find-k-pairs-with-smallest-sums/
 https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
 https://leetcode.com/problems/kth-smallest-number-in-multiplication-table/
 https://leetcode.com/problems/k-th-smallest-prime-fraction/
