bd0c92b34a7d0cd73cd13b208116b8a9f9f6372c
85937023 <85937023@qq.com>
2019-07-20 11:09:56 +0800

    786 ac

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 https://leetcode.com/problems/search-a-2d-matrix-ii/
 https://leetcode.com/problems/search-insert-position/
 https://leetcode.com/problems/kth-smallest-number-in-multiplication-table/
-https://leetcode.com/problems/k-th-smallest-prime-fraction/
 https://leetcode.com/problems/split-array-largest-sum/
 https://leetcode.com/problems/swim-in-rising-water/
 
--- array/binary search/786. K-th Smallest Prime Fraction.py
+++ array/binary search/786. K-th Smallest Prime Fraction.py
@@ -8,39 +8,37 @@ def kthSmallestPrimeFraction(A, K):
             lo = 0
             hi = i-1
             while lo<hi:
-                mid = (lo+hi)/2
-                curVal = 1.0*AA[mid]/AA[i]
-                if abs(curVal-val)<=err:
+                mid = (lo+hi+1)//2
+                #land on bigger between two
+                if AA[mid]/AA[i]<val:
                     lo = mid
-                    break
-                elif curVal<val:
-                    lo = mid+1
                 else:
                     hi = mid-1
-
-            cnt += lo
+            if lo == hi:
+                cnt += lo+1 if A[lo]/A[i] < val else lo
         return cnt
 
     lens = len(A)
-    lo = 1.0/A[-1]
+    lo = 1/A[-1]
     hi = (A[-1]-1)/A[-1]
-    while hi-lo < err:
+    while hi-lo > err:
         mid = (hi+lo)/2
         cnt = smaller(A, lens, mid)
+        #approach to real fraction as many as possible
         if cnt > K-1:
-            hi = mid-1
+            hi = mid
         else:
-            lo = mid+1
+            lo = mid
 
+    print(lo)
     for i in range(lens):
-        p = int(math.ceil(A[i]*lo))
-        print(p)
-
+        p = int(math.floor(A[i]*lo+0.5))
+        #print(p, A[i], abs(p/A[i]-lo))
         if (p < A[i]) and (p in A) and (abs(p/A[i]-lo) < err):
             return [p,A[i]]
 
     return []
 
-A = [1, 2, 3, 5]
+A = [1,2,11,37,83,89]
 K = 3
 print(kthSmallestPrimeFraction(A, K))
