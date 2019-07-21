1f09abc5b00c51beb87e81e233d7b6d05a208ce7
85937023 <85937023@qq.com>
2019-07-21 15:15:17 +0800

    1011 ac

--- __init__.py
+++ __init__.py
@@ -3,11 +3,6 @@ https://leetcode.com/problems/kth-smallest-number-in-multiplication-table/
 https://leetcode.com/problems/split-array-largest-sum/
 https://leetcode.com/problems/swim-in-rising-water/
 
-https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
-https://leetcode.com/problems/time-based-key-value-store/
-https://leetcode.com/problems/find-the-duplicate-number/
-https://leetcode.com/problems/koko-eating-bananas/
-https://leetcode.com/problems/find-right-interval/
 
 catetory:
 373. Find K Pairs with Smallest Sums
