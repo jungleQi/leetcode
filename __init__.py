b26a2bc54f4bb8642fd88973a54a02b949965abc
jungle <jungle@alibaba-inc.com>
2019-07-30 20:05:10 +0800

    .

--- __init__.py
+++ __init__.py
@@ -1,6 +1,15 @@
-catetory:
+binaray search catetory:
 373. Find K Pairs with Smallest Sums
 378. Kth Smallest Element in a Sorted Matrix
 668. Kth Smallest Number in Multiplication Table
 719. Find K-th Smallest Pair Distance
 786. K-th Smallest Prime Fraction
+
+
+combination:
+https://leetcode.com/problems/combination-sum/
+https://leetcode.com/problems/combination-sum-ii/
+https://leetcode.com/problems/combination-sum-iii/
+https://leetcode.com/problems/combination-sum-iv/
+https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+https://leetcode.com/problems/generate-parentheses/
