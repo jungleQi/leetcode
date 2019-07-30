93e3eaab90eb21f695824ee050bb9b197feb921d
jungle <jungle@alibaba-inc.com>
2019-07-30 19:56:32 +0800

    668 ac

--- __init__.py
+++ __init__.py
@@ -1,9 +1,3 @@
-https://leetcode.com/problems/search-a-2d-matrix-ii/
-https://leetcode.com/problems/kth-smallest-number-in-multiplication-table/
-https://leetcode.com/problems/split-array-largest-sum/
-https://leetcode.com/problems/swim-in-rising-water/
-
-
 catetory:
 373. Find K Pairs with Smallest Sums
 378. Kth Smallest Element in a Sorted Matrix
